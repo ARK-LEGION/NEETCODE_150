a3663e4e5154311a0c223f61d6ab7319d6f837a4
ARJUN SHENDE <cs1240252@iitd.ac.in>
2026-06-12 07:43:23 +0530

    Add: target-sum - submission-3

---
+++ Data Structures & Algorithms/target-sum/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        target+=1000;
+        vector<vector<int>> dp(n+1, vector<int>(20001,0));
+        dp[0][1000]=1;
+        for (int i=1; i<=n; i++){
+            for (int j=0; j<=2e4 ;j++){
+                dp[i][j] = (j-nums[i-1]>=0 ? dp[i-1][j-nums[i-1]] : 0) + (j+nums[i-1]<=2e4 ? dp[i-1][j+nums[i-1]] : 0);
+            }
+        }
+        return dp[n][target];
+    }
+};
