4db15065312ba8e400f912358e402089831e35f1
ARJUN SHENDE <cs1240252@iitd.ac.in>
2026-06-16 08:19:30 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<double,vector<int>>> pq;
+        for (int i=0; i<points.size(); i++){
+            double d = sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1]);
+            if (pq.size()<k){
+                pq.push({d,points[i]});
+            }
+            else {
+                if (d < pq.top().first){
+                    pq.pop();
+                    pq.push({d,points[i]});
+                }
+            }
+        }
+        vector<vector<int>> ans;
+        while (!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
